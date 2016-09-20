f1a48fd86ca31afa9c1172dcbfbd4a2ffeafc853
André Luiz Pergentino da Silva <andrepgsilva@outlook.com>
2016-09-19 22:31:41 -0300

    Retirando códigos comentados

--- simpleweb.py
+++ simpleweb.py
@@ -12,11 +12,6 @@ def web_input(name_field, fields_values=fields_and_values):
 def web_env(fields=fields_and_values):
     result_ready = False if None in fields.values() else True
     return {"fields":fields, "result_ready":result_ready}
-    # return {"fields":fields, "form":form, "result_ready":result_ready, "web_print":web_print()}
-
-
-# def web_print(fields_values=fields_and_values):
-#     return sum(fields_and_values.values())
 
 
 def web_run(application = app):
